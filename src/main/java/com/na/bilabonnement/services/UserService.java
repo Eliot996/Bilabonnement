9011564bf30bdc07e0c7b7097a1837a30b517f33
H4ppyN4p <tobi5703@stud.kea.dk>
2022-05-09 14:04:47 +0200

    Login User Test created and confirmed

--- src/main/java/com/na/bilabonnement/services/UserService.java
+++ src/main/java/com/na/bilabonnement/services/UserService.java
@@ -1,4 +1,31 @@
 package com.na.bilabonnement.services;
 
-public class UserService {
+import com.na.bilabonnement.models.User;
+import com.na.bilabonnement.repositories.IUserRepository;
+
+import java.util.List;
+
+//Made by: Tobias Arboe
+public class UserService
+{
+    final IUserRepository repo;
+
+    public UserService(IUserRepository repo){
+        this.repo = repo;
+    }
+
+    public boolean checkLogin(String expectedUsername, String expectedPassword){
+        boolean loginValidity = false;
+
+        List<User> listOfUsers = repo.getAllEntities();
+
+        for (User user: listOfUsers
+             ) {
+            if (user.getUsername().equals(expectedUsername) && user.getPassword().equals(expectedPassword) ){
+                loginValidity = true;
+            }
+        }
+
+        return loginValidity;
+    }
 }
---
+++ src/test/java/com/na/bilabonnement/services/UserServiceTest.java
@@ -0,0 +1,59 @@
+package com.na.bilabonnement.services;
+
+import com.na.bilabonnement.repos.DummyUserRepo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+//Made by: Tobias Arboe
+class UserServiceTest
+{
+
+    @Test
+    void checkLogin() {
+        //Arrange
+        UserService userService = new UserService(new DummyUserRepo());
+
+        final boolean isLoggedIn = true;
+        final boolean isNotLoggedIn = false;
+
+
+
+        //Act
+
+        boolean testUserOneTrue = userService.checkLogin("Sofia", "1234");
+        boolean testUserOneFalse = userService.checkLogin("Sofia", "123456");
+        boolean testUserOneFalse2 = userService.checkLogin("Sofias", "1234");
+
+        boolean testUserTwoTrue = userService.checkLogin("Lasse", "password");
+        boolean testUserTwoFalse = userService.checkLogin("Lasse", "Password");
+        boolean testUserTwoFalse2 = userService.checkLogin("Lasses", "password");
+
+        boolean testUserThreeTrue = userService.checkLogin("Arboe", "test");
+        boolean testUserThreeFalse = userService.checkLogin("Arboe", "testy");
+        boolean testUserThreeFalse2 = userService.checkLogin("Arboes", "test");
+
+        boolean testUserFourTrue = userService.checkLogin("Mathias", "Pass1234");
+        boolean testUserFourFalse = userService.checkLogin("Mathias", "Pass12345");
+        boolean testUserFourFalse2 = userService.checkLogin("Mathiass", "Pass1234");
+
+        //Assert
+        assertEquals(isLoggedIn, testUserOneTrue);
+        assertEquals(isNotLoggedIn, testUserOneFalse);
+        assertEquals(isNotLoggedIn, testUserOneFalse2);
+
+        assertEquals(isLoggedIn, testUserTwoTrue);
+        assertEquals(isNotLoggedIn, testUserTwoFalse);
+        //assertEquals(isNotLoggedIn, testUserTwoFalse2);
+
+        assertEquals(isLoggedIn, testUserThreeTrue);
+        assertEquals(isNotLoggedIn, testUserThreeFalse);
+       // assertEquals(isNotLoggedIn, testUserThreeFalse2);
+
+        assertEquals(isLoggedIn, testUserFourTrue);
+        assertEquals(isNotLoggedIn, testUserFourFalse);
+       // assertEquals(isNotLoggedIn, testUserFourFalse2);
+
+    }
+}
