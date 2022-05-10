4971344d5ac14d9650f10f1e1879e09a7fa40e78
Eliot996 <math10t6@stud.kea.dk>
2022-05-10 11:35:17 +0200

    implemented as singleton

--- src/main/java/com/na/bilabonnement/repositories/UserRepo.java
+++ src/main/java/com/na/bilabonnement/repositories/UserRepo.java
@@ -5,6 +5,12 @@ import com.na.bilabonnement.models.User;
 import java.util.List;
 
 public class UserRepo implements IUserRepository {
+    private static final UserRepo instance = new UserRepo();
+    private UserRepo() {}
+    public static UserRepo getInstance() {
+        return instance;
+    }
+
     @Override
     public User create(User entity) {
         return null;
