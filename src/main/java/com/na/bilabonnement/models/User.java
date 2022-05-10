b1f2637efd3704df04d0d50e9c380ee015cc07b0
Eliot996 <math10t6@stud.kea.dk>
2022-05-10 10:17:35 +0200

    added variables and empty constructor

--- src/main/java/com/na/bilabonnement/models/User.java
+++ src/main/java/com/na/bilabonnement/models/User.java
@@ -1,21 +1,64 @@
 package com.na.bilabonnement.models;
 
 
-//Made by: Tobias Arboe
+/**
+ *  @author Tobias Arboe
+ *  @author Mathias(Eliot996)
+*/
+
 public class User {
-    private String username = "";
-    private String password = "";
+    private String username;
+    private String password;
+    private String salt;
+    private UserRole role;
+    private int locationId;
 
     public User(String username, String password){
         this.username = username;
         this.password = password;
     }
 
+    public User() {
+
+    }
+
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
     }
 }
