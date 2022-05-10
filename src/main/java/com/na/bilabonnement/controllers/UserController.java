fe50ecc800d3d7ef7c3e7306e58f4ce96b569de6
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2022-05-10 13:07:24 +0200

    small changes

--- src/main/java/com/na/bilabonnement/controllers/UserController.java
+++ src/main/java/com/na/bilabonnement/controllers/UserController.java
@@ -6,16 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
+    /*
+    @Author Sofia
+     */
+
+
     @GetMapping("/admin")
     public String admin(){
         return "administratorpage";
     }
 
+    /*
+    @Author Sofia
+     */
     @GetMapping("/dataregistrering")
     public String dataregistration(){
         return "dataregistration";
     }
 
+    /*
+    @Author Sofia
+     */
     @GetMapping("skade_og_udbedring")
     public String damageAndRectification(){
         return "damageAndRectification";
