ba3ccdad13a23f517ba0873abe4e87c7c5c950a0
Eliot996 <math10t6@stud.kea.dk>
2022-05-10 12:28:43 +0200

    added create user

--- src/main/java/com/na/bilabonnement/controllers/UserController.java
+++ src/main/java/com/na/bilabonnement/controllers/UserController.java
@@ -1,7 +1,40 @@
 package com.na.bilabonnement.controllers;
 
+import com.na.bilabonnement.models.User;
+import com.na.bilabonnement.services.LocationService;
+import com.na.bilabonnement.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class UserController {
 
+    private UserService userService = new UserService();
+    private LocationService locationService = new LocationService();
 
+    /**
+    *  @author Mathias(Eliot996)
+    */
+    @GetMapping("/create-user")
+    public String getCreateUser(HttpSession session, Model model) {
+        model.addAttribute("locations", locationService.getAllLocations());
+        model.addAttribute("user", new User());
+        return "create-user";
+    }
 
+    /**
+    *  @author Mathias(Eliot996)
+    */
+    @PostMapping("/create-user")
+    public String CreateUser(HttpSession session, @ModelAttribute User user) {
+
+        userService.createUser(user.getUsername(), user.getPassword(), user.getRoleID(), user.getLocationId());
+
+        return "create-user";
+    }
 }
