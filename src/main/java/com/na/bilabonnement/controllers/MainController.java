9fca81b2485fcd4675a905c3294b8d96b36c123c
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2022-05-10 10:20:05 +0200

    html og css ting

--- src/main/java/com/na/bilabonnement/controllers/MainController.java
+++ src/main/java/com/na/bilabonnement/controllers/MainController.java
@@ -2,7 +2,9 @@ package com.na.bilabonnement.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+/*
+@Author Sofia
+ */
 @Controller
 public class MainController {
 @GetMapping("/")
