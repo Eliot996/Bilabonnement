6f769523ed3927c0513b4d80dc18aeb1b5c01a15
lass026q <lass026q@stud.kea.dk>
2022-05-10 11:08:17 +0200

    start of admin page

--- src/main/java/com/na/bilabonnement/controllers/UserController.java
+++ src/main/java/com/na/bilabonnement/controllers/UserController.java
@@ -1,7 +1,13 @@
 package com.na.bilabonnement.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
 
-
-
+    @GetMapping("/admin")
+    public String admin(){
+        return "administratorpage";
+    }
 }
