8ade17b8324d91f3959c9951aa67b467e590fdf9
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2022-05-16 11:58:30 +0200

    DDL done for cars

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -26,3 +26,27 @@ CREATE TABLE `bilabonnement`.`Users` (
                                                 ON DELETE NO ACTION
                                                 ON UPDATE NO ACTION);
 
+
+CREATE TABLE `bilabonnement`.`cars` (
+                                        `id` INT NOT NULL,
+                                        `chassisNumber` VARCHAR(17) NULL,
+                                        `status` VARCHAR(255) NULL,
+                                        `make` VARCHAR(255) NULL,
+                                        `model` VARCHAR(255) NULL,
+                                        `trimLevel` VARCHAR(2400) NULL,
+                                        `scrapPrice` INT NULL,
+                                        `registrationFee` INT NULL,
+                                        `co2Emission` VARCHAR(255) NULL,
+                                        `kilometersDriven` INT NULL,
+                                        `damages` VARCHAR(2400) NULL,
+                                        `colour` VARCHAR(255) NULL,
+                                        `fuelType` VARCHAR(255) NULL,
+                                        `locationId` INT NULL,
+                                        PRIMARY KEY (`id`),
+                                        UNIQUE INDEX `chassisNumber_UNIQUE` (`chassisNumber` ASC) VISIBLE,
+                                        CONSTRAINT `locationId_FK`
+                                            FOREIGN KEY (`id`)
+                                                REFERENCES `bilabonnement`.`locations` (`id`)
+                                                ON DELETE NO ACTION
+                                                ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
