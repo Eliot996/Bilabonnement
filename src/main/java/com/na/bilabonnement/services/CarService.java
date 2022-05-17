eab3805f822d32789eacf0d85c0fac0c08dc00c9
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2022-05-17 10:53:55 +0200

    carservice

--- src/main/java/com/na/bilabonnement/services/CarService.java
+++ src/main/java/com/na/bilabonnement/services/CarService.java
@@ -1,4 +1,30 @@
 package com.na.bilabonnement.services;
 
+import com.na.bilabonnement.models.Car;
+import com.na.bilabonnement.repositories.CarRepo;
+import com.na.bilabonnement.repositories.ICarRepository;
+import com.na.bilabonnement.repositories.IUserRepository;
+
 public class CarService {
+
+
+    public Car createCar(int id, int chassisNumber, boolean status, String make, String trimLevel,
+                         int scrapPrice, int registrationFee, int co2Emission, int kilometersDriven,
+                         String damage, String colour, String fuelType, int locationId){
+        Car car = new Car(
+                id,
+                chassisNumber,
+                status,
+                make,
+                trimLevel,
+                scrapPrice,
+                registrationFee,
+                co2Emission,
+                kilometersDriven,
+                damage,
+                colour,
+                fuelType,
+                locationId);
+        return repo.create(car);
+    }
 }
