757e7cdd783f5df076a85d36558841a1ba8bf3c7
Eliot996 <math10t6@stud.kea.dk>
2022-05-17 11:55:58 +0200

    minor fix

--- src/main/java/com/na/bilabonnement/services/CarService.java
+++ src/main/java/com/na/bilabonnement/services/CarService.java
@@ -10,7 +10,7 @@ public class CarService {
         this.repo = repo;
     }
 
-    public Car createCar(int id, int chassisNumber, boolean status, String make, String trimLevel,
+    public Car createCar(int id, int chassisNumber, String status, String make, String trimLevel,
                          int scrapPrice, int registrationFee, int co2Emission, int kilometersDriven,
                          String damage, String colour, String fuelType, int locationId){
         Car car = new Car(
