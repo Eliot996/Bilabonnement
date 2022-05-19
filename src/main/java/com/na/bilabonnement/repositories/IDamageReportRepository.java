f86ec947f2e7bd53bee7e77f19980908b3ccd7bb
lass026q <lass026q@stud.kea.dk>
2022-05-19 12:06:30 +0200

    repo

---
+++ src/main/java/com/na/bilabonnement/repositories/IDamageReportRepository.java
@@ -0,0 +1,8 @@
+package com.na.bilabonnement.repositories;
+
+import com.na.bilabonnement.models.DamageReport;
+import com.na.bilabonnement.models.DamageReportLine;
+
+public interface IDamageReportRepository extends IRepository <DamageReport>
+{
+}
