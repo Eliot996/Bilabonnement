00c78fc43a7d6812f501e883c54c0751c6beade2
Eliot996 <math10t6@stud.kea.dk>
2022-05-23 13:59:10 +0200

    added getFile

--- src/main/java/com/na/bilabonnement/services/RentalAgreementService.java
+++ src/main/java/com/na/bilabonnement/services/RentalAgreementService.java
@@ -1,9 +1,12 @@
 package com.na.bilabonnement.services;
 
+import com.na.bilabonnement.models.FileReply;
 import com.na.bilabonnement.models.RentalAgreement;
 import com.na.bilabonnement.repositories.interfaces.IRentalAgreementRepository;
 import com.na.bilabonnement.repositories.RentalAgreementRepo;
+import org.springframework.core.io.Resource;
 
+import java.io.File;
 import java.util.List;
 
 public class RentalAgreementService {
@@ -50,5 +53,9 @@ public class RentalAgreementService {
         RentalAgreement ra = repo.getSingleEntityById(id);
         ra.setCar(carService.getCar(ra.getCarId()));
         return ra;
+    }
+
+    public FileReply getFile(int id) {
+        return repo.loadFile(id);
     }
 }
