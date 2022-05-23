063fd84fb4f4d732e4107725eb44f3c120834571
Eliot996 <math10t6@stud.kea.dk>
2022-05-23 12:52:59 +0200

    added updateWithoutContract

--- src/main/java/com/na/bilabonnement/repositories/interfaces/IRentalAgreementRepository.java
+++ src/main/java/com/na/bilabonnement/repositories/interfaces/IRentalAgreementRepository.java
@@ -3,4 +3,5 @@ package com.na.bilabonnement.repositories.interfaces;
 import com.na.bilabonnement.models.RentalAgreement;
 
 public interface IRentalAgreementRepository extends IRepository<RentalAgreement>{
+    RentalAgreement updateWithoutContract(RentalAgreement entity);
 }
