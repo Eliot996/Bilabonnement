74869a4e3d82dd6a8e6e32b8c3ea71a1e31a45c7
lass026q <lass026q@stud.kea.dk>
2022-05-23 10:54:31 +0200

    mapping for damagereportlines, og html sider

--- src/main/java/com/na/bilabonnement/controllers/DamageReportController.java
+++ src/main/java/com/na/bilabonnement/controllers/DamageReportController.java
@@ -1,9 +1,12 @@
 package com.na.bilabonnement.controllers;
 
 import com.na.bilabonnement.models.DamageReport;
+import com.na.bilabonnement.models.DamageReportLine;
+import com.na.bilabonnement.models.User;
 import com.na.bilabonnement.models.UserRole;
 import com.na.bilabonnement.repositories.CarRepo;
 import com.na.bilabonnement.services.CarService;
+import com.na.bilabonnement.services.DamageReportLineService;
 import com.na.bilabonnement.services.DamageReportService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +23,7 @@ public class DamageReportController {
      */
     private final DamageReportService DAMAGE_REPORT_SERVICE = new DamageReportService();
     private final CarService CAR_SERVICE = new CarService();
+    private final DamageReportLineService DAMAGE_REPORT_LINE_SERVICE = new DamageReportLineService();
 
     @GetMapping("/opret-skadesrapport")
     public String getCreateDamageReport(HttpSession session, Model model){
@@ -75,5 +79,68 @@ public class DamageReportController {
         DAMAGE_REPORT_SERVICE.deleteDamageReport(damageReportId);
         return "redirect:/skadesrapporter";
     }
+
+
+    @GetMapping("/opret-skade")
+    public String getCreateDamageReportLine(HttpSession session, Model model){
+        UserRole userRole = (UserRole)session.getAttribute("userRole");
+        model.addAttribute("userRole", userRole.toString());
+
+        model.addAttribute("damageReportLine", new DamageReportLine());
+        model.addAttribute("damageReports", DAMAGE_REPORT_SERVICE.getAllDamageReports());
+        return "create-damage-report-line";
+    }
+
+
+    @PostMapping("/opret-skade")
+    public String createDamageReportLine(HttpSession session, Model model, @ModelAttribute DamageReportLine damageReportLine){
+        DamageReportLine createdDamageReportLine = DAMAGE_REPORT_LINE_SERVICE.createDamageReportLine(damageReportLine);
+        return "redirect:/skadesrapport/" + createdDamageReportLine.getLineNumber();
+    }
+
+
+    @GetMapping("/skader")
+    public String viewAllDamageReportLines(HttpSession session, Model model){
+        UserRole userRole = (UserRole) session.getAttribute("userRole");
+        model.addAttribute("userRole", userRole.toString());
+
+        model.addAttribute("listOfDamageReportLines", DAMAGE_REPORT_LINE_SERVICE.getAllDamageReportLines());
+
+        return "all-damage-report-lines";
+    }
+
+    @GetMapping("/skade/{lineNumber}")
+    public String getEditDamageReportLine(HttpSession session, @PathVariable() int lineNumber, Model model){
+        UserRole userRole = (UserRole) session.getAttribute("userRole");
+        if (userRole != UserRole.DAMAGE_AND_RECTIFICATION){
+            return "redirect:/skader";
+
+        }
+
+        model.addAttribute("userRole", userRole.toString());
+
+        DamageReportLine damageReportLine = DAMAGE_REPORT_LINE_SERVICE.getDamageReportLine(lineNumber);
+        model.addAttribute("damageReportLine", damageReportLine);
+        return "edit-damage-report-line";
+    }
+
+
+    @PostMapping("/skade/{lineNumber}")
+    public String editDamageReport(HttpSession session, @ModelAttribute DamageReportLine damageReportLine, @PathVariable int lineNumber){
+        DAMAGE_REPORT_LINE_SERVICE.updateDamageReportLine(damageReportLine.getLineNumber(), damageReportLine.getDamageReportId(), damageReportLine.getDamageNotes(), damageReportLine.getPrice());
+        return "redirect:/skader";
+    }
+
+
+
+    @GetMapping("/skade/{lineNumber}/slet")
+    public String editDamageReportLine(HttpSession session, @PathVariable() int lineNumber, Model model){
+        UserRole userRole = (UserRole) session.getAttribute("userRole");
+        model.addAttribute("userRole", userRole);
+
+        DAMAGE_REPORT_LINE_SERVICE.deleteDamageReportLine(lineNumber);
+        return "redirect:/skader";
+    }
+
 }
 
