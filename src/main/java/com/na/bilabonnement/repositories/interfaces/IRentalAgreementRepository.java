2db3d3e61a155feb598d1d056f798d600a70e27d
Eliot996 <math10t6@stud.kea.dk>
2022-05-23 16:02:41 +0200

    removed unused import

--- src/main/java/com/na/bilabonnement/repositories/interfaces/IRentalAgreementRepository.java
+++ src/main/java/com/na/bilabonnement/repositories/interfaces/IRentalAgreementRepository.java
@@ -2,7 +2,6 @@ package com.na.bilabonnement.repositories.interfaces;
 
 import com.na.bilabonnement.models.FileReply;
 import com.na.bilabonnement.models.RentalAgreement;
-import org.springframework.core.io.Resource;
 
 public interface IRentalAgreementRepository extends IRepository<RentalAgreement>{
     RentalAgreement updateWithoutContract(RentalAgreement entity);
