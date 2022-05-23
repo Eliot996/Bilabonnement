589e37c3d0554821defe06a40bf2d616d6bb58c4
Eliot996 <math10t6@stud.kea.dk>
2022-05-23 13:57:49 +0200

    added loadFile

--- src/main/java/com/na/bilabonnement/repositories/interfaces/IRentalAgreementRepository.java
+++ src/main/java/com/na/bilabonnement/repositories/interfaces/IRentalAgreementRepository.java
@@ -1,7 +1,11 @@
 package com.na.bilabonnement.repositories.interfaces;
 
+import com.na.bilabonnement.models.FileReply;
 import com.na.bilabonnement.models.RentalAgreement;
+import org.springframework.core.io.Resource;
 
 public interface IRentalAgreementRepository extends IRepository<RentalAgreement>{
     RentalAgreement updateWithoutContract(RentalAgreement entity);
+
+    FileReply loadFile(int id);
 }
