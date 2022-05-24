5d95e42b031c27b810026fb64200f938b1223440
Eliot996 <math10t6@stud.kea.dk>
2022-05-24 10:30:37 +0200

    tests for update done

--- src/test/java/com/na/bilabonnement/services/RentalAgreementTest.java
+++ src/test/java/com/na/bilabonnement/services/RentalAgreementTest.java
@@ -5,8 +5,11 @@ import com.na.bilabonnement.models.RentalType;
 import com.na.bilabonnement.repositories.DummyCarRepo;
 import com.na.bilabonnement.repositories.DummyRentalAgreementRepo;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -80,5 +83,57 @@ public class RentalAgreementTest {
         assertEquals(list.get(0).getCar().getId(), list.get(0).getCarId());
         assertEquals(list.get(1).getCar().getId(), list.get(1).getCarId());
         assertEquals(list.get(2).getCar().getId(), list.get(2).getCarId());
+    }
+
+    @Test
+    void update_rentalAgreement_withFile() {
+        // Arrange
+        CarService CAR_SERVICE = new CarService();
+        CAR_SERVICE.setRepo(new DummyCarRepo());
+
+        RentalAgreementService RA_SERVICE = new RentalAgreementService();
+        RA_SERVICE.setRepo(new DummyRentalAgreementRepo());
+        RA_SERVICE.setCarService(CAR_SERVICE);
+
+        RA_SERVICE.create( new RentalAgreement(-1, 1, 3500, LocalDate.of(2020, 10, 1),
+                LocalDate.of(2022, 10, 1), RentalType.UNLIMITED) );
+
+        MultipartFile file = new MockMultipartFile("file", "filename.txt", "text/plain", "some text".getBytes());
+
+        RentalAgreement ra = RA_SERVICE.get(0);
+
+        ra.setContract(file);
+
+        // Act
+        RA_SERVICE.update(ra);
+
+        // Assert
+        assertEquals(1, ra.getTypeId());
+    }
+
+    @Test
+    void update_rentalAgreement_withoutFile() {
+        // Arrange
+        CarService CAR_SERVICE = new CarService();
+        CAR_SERVICE.setRepo(new DummyCarRepo());
+
+        RentalAgreementService RA_SERVICE = new RentalAgreementService();
+        RA_SERVICE.setRepo(new DummyRentalAgreementRepo());
+        RA_SERVICE.setCarService(CAR_SERVICE);
+
+        RA_SERVICE.create( new RentalAgreement(-1, 1, 3500, LocalDate.of(2020, 10, 1),
+                LocalDate.of(2022, 10, 1), RentalType.UNLIMITED) );
+
+        MultipartFile file = new MockMultipartFile("null", "null", "null", "".getBytes());
+
+        RentalAgreement ra = RA_SERVICE.get(0);
+
+        ra.setContract(file);
+
+        // Act
+        RA_SERVICE.update(ra);
+
+        // Assert
+        assertEquals(2, ra.getTypeId());
     }
 }
