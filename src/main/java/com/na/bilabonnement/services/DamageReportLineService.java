78963eb16f40905704da40703a3536b5a09e8e66
Eliot996 <math10t6@stud.kea.dk>
2022-05-26 14:10:21 +0200

    Added authors

--- src/main/java/com/na/bilabonnement/services/DamageReportLineService.java
+++ src/main/java/com/na/bilabonnement/services/DamageReportLineService.java
@@ -6,23 +6,29 @@ import com.na.bilabonnement.repositories.interfaces.IDamageReportLineRepository;
 
 import java.util.List;
 
-/*
-@Author Sofia
- */
 public class DamageReportLineService {
     private IDamageReportLineRepository repo = DamageReportLineRepo.getInstance();
     public void setRepo(IDamageReportLineRepository repo){
         this.repo = repo;
     }
 
+    /**
+     *  @author Sofia
+     */
     public DamageReportLine createDamageReportLine(DamageReportLine damageReportLine){
         return repo.create(damageReportLine);
     }
 
+    /**
+     *  @author Sofia
+     */
     public boolean deleteDamageReportLine(int lineNumber, int damageReportId){
         return repo.deleteById(lineNumber,damageReportId);
     }
 
+    /**
+     *  @author Sofia
+     */
     public DamageReportLine updateDamageReportLine(int damageReportLineId, int lineNumber, String damageNotes, int price){
         DamageReportLine damageReportLine = repo.getSingleEntityById(damageReportLineId);
         damageReportLine.setDamageReportId(damageReportLineId);
@@ -33,10 +39,17 @@ public class DamageReportLineService {
         return repo.update(damageReportLine);
     }
 
+    /**
+     *  @author Sofia
+     *  @author Mathias(Eliot996)
+     */
     public DamageReportLine getDamageReportLine(int id, int damageReportId){
         return repo.getSingleEntityByLinenumberAndDamageReportId(id, damageReportId);
     }
 
+    /**
+     *  @author Sofia
+     */
     public List<DamageReportLine> getAllDamageReportLinesWithReportId(int damageReportId){
         return repo.getAllEntitiesWithDamageReportId(damageReportId);
     }
